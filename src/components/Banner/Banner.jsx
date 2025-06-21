ec90ab8ef0f114c7b9cc3b9fef6a730903202eb7
EmHiuDemacia <hieumai1507@gmail.com>
2025-06-21 07:54:27 +0700

    fix: ui

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,15 +3,15 @@ import styles from './styles.module.scss';
 import Button from '@components/Button/Button';
 
 function Banner() {
-  const { container, content, title, desc, button} = styles;
+  const { container, content, title, desc, button } = styles;
   return (
     <div className={container}>
       <div className={content}>
-        <h1 className={title}>XStore Marseille04 Demo</h1>
+        <h1 className={title}>XStore Marseille04</h1>
         <p className={desc}>
           Make yours celebrations even more special this years with beautiful.
         </p>
-        <Button content={'Go to shop'}/>
+        <Button content={'Go to shop'} />
       </div>
     </div>
   );
